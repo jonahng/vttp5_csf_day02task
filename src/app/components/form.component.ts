9c0e82b09fec55a067de04a84d7e952087695e41
jonahng <nyhj1998@gmail.com>
2025-02-21 14:58:18 +0800

    validation and orders

--- src/app/components/form.component.ts
+++ src/app/components/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormModel } from '../models';
 
 @Component({
@@ -13,6 +13,7 @@ export class FormComponent implements OnInit{
 
   private fb = inject(FormBuilder)
   protected form!: FormGroup
+  protected orders!: FormArray
 
   ngOnInit(): void {
     this.form = this.createForm()
@@ -24,17 +25,45 @@ export class FormComponent implements OnInit{
     console.log("Form Data:  ", values )
   }
 
+  protected addOrder(){
+    const order = this.createOrder()
+    this.orders.push(order)
+  }
+
+  protected removeOrder(idx: number){
+    this.orders.removeAt(idx)
+  }
+
+  protected isValid(ctrlName:string): boolean{
+    return !!this.form.get(ctrlName)?.valid
+  }
+
+  protected isCtrlInvalid(ctrlName:string): boolean{
+    return !!this.form.get(ctrlName)?.invalid
+  }
+
 
   private createForm(): FormGroup{
+    this.orders=this.fb.array([])
     const form = this.fb.group({
-     name: this.fb.control<string>(''),
+     name: this.fb.control<string>('',[Validators.required, Validators.minLength(3)]),
      address: this.fb.control<string>(''),
      email: this.fb.control<string>(''),
     deliveryDate: this.fb.control<string>(''),
     urgency: this.fb.control<boolean>(false),
+    orders: this.orders
 
     })
     return form
+  }
+
+
+  private createOrder(): FormGroup{
+    return this.fb.group({
+      item: this.fb.control<string>(''),
+      quantity: this.fb.control<string>(''),
+      unitPrice: this.fb.control<string>('')
+    })
   }
 
 }
